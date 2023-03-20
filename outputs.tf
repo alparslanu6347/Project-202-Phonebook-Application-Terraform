a5fcc9879d823b37623fe66d9f0b1a5d473930b4
arrow <alparslanu6347@gmail.com>
2023-03-20 09:27:17 +0300

    output updated and resource_7-github_repository_file.tf added

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,8 @@
-
 output "application-load-balancer_dns" {
   description = "Phonebook Application Load Balancer Dns Name"
   value       = "http://${aws_alb.webserver-alb.dns_name}"
+}
+
+output "db_instance_address" {
+  value = aws_db_instance.webserver-db.address
 }
---
+++ resource_7-github_repository_file.tf
@@ -0,0 +1,19 @@
+resource "github_repository_file" "webserver-db_endpoint" {
+  content             = aws_db_instance.webserver-db.address
+  file                = "dbserver.endpoint"
+  repository          = "Project-202-Phonebook-Application-Terraform"
+  overwrite_on_create = true
+  branch              = "main"
+  depends_on          = [aws_db_instance.webserver-db]
+}
+
+
+
+
+
+
+
+
+
+
+
