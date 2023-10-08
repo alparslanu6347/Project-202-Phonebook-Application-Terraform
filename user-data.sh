9fc07fb759ce2df0ccde2d05d551c2fbe445f296
arrow <alparslanu6347@gmail.com>
2023-10-08 13:49:46 +0300

    files updated

--- user-data.sh
+++ user-data.sh
@@ -5,8 +5,8 @@ yum install pip -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN=${user-data-git-token}
-USER=${user-data-git-name}
+TOKEN=${user-data-git-token}    # main.tf --> data "template_file" "phonebook" -->> user-data-git-token = var.git-token
+USER=${user-data-git-name}      # main.tf --> data "template_file" "phonebook" -->> user-data-git-name  = var.git-name
 cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git    # change your repo name
 python3 /home/ec2-user/phonebook/phonebook-app.py
 
