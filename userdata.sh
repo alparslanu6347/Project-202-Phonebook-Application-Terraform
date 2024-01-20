67d40fa2052b05edd0bd8a9b510e297ab76dfddb
alparslanu6347 <alparslanu6347@gmail.com>
2024-01-21 00:07:11 +0300

    userdata.sh updated

--- userdata.sh
+++ userdata.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-
 dnf update -y
 dnf install python3 -y
 dnf install python3-pip -y
 pip3 install flask
 pip3 install flask_mysql
 dnf install git -y
-TOKEN=${userdata-git-token}
-USER=${userdata-git-name}
-cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git
+TOKEN=${userdata-git-token} # main.tf --> data "template_file" "phonebook" -->> userdata-git-token = var.git-token
+USER=${userdata-git-name}   # main.tf --> data "template_file" "phonebook" -->> userdata-git-name  = var.git-name
+cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git    # change your repo name
 python3 /home/ec2-user/phonebook/phonebook-app.py
 
 
